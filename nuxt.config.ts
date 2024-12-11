6fdb72d494342901ffebdc698034782295322cf8
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-12-11 10:02:50 +0200

    Added pinia store to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+  ],
   ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
