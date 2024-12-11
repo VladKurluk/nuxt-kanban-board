5c42cb3ff939c549201bfe787ba9cd07f1f661ae
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-12-11 17:46:49 +0200

    Added form validation

--- components/NewTask.vue
+++ components/NewTask.vue
@@ -1,53 +1,57 @@
 <template>
   <UModal>
-    <UCard
-      :ui="{
-        ring: '',
-        divide: 'divide-y divide-gray-100 dark:divide-gray-800' 
-      }"
-    >
-      <template #header>
-        <UInput
-          color="primary"
-          variant="outline"
-          placeholder="Title"
-          v-model="task.title"
-        />
-      </template>
+    <UCard>
+      <UForm
+        :schema="schema"
+        :state="task"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <UFormGroup label="Title" name="title">
+          <UInput
+            color="primary"
+            variant="outline"
+            placeholder="Title"
+            v-model="task.title"
+          />
+        </UFormGroup>
 
-      <div>
-        <UTextarea
-          color="primary"
-          variant="outline"
-          placeholder="Description"
-          v-model="task.description"
-        />
+        <UFormGroup label="Description" name="description">
+          <UTextarea
+            color="primary"
+            variant="outline"
+            placeholder="Description"
+            v-model="task.description"
+          />
+        </UFormGroup>
+
         <div class="flex justify-between py-2.5 gap-2.5">
           <div class="w-1/2">
-            <p class="mb-2">Status:</p>
-            <USelect
-              color="primary"
-              variant="outline"
-              :options="['TODO', 'In progress', 'Done']"
-              v-model="task.status" 
-            />
+            <UFormGroup label="Status:" name="status">
+              <USelect
+                color="primary"
+                variant="outline"
+                :options="['TODO', 'In progress', 'Done']"
+                v-model="task.status" 
+              />
+            </UFormGroup>   
           </div>
           <div class="w-1/2">
-            <p class="mb-2">Priority:</p>
-            <USelect
-              color="primary"
-              variant="outline"
-              :options="['Low', 'Medium', 'High']"
-              v-model="task.priority"
-            />
+            <UFormGroup label="Priority:" name="priority">
+              <USelect
+                color="primary"
+                variant="outline"
+                :options="['Low', 'Medium', 'High']"
+                v-model="task.priority"
+              />
+            </UFormGroup>            
           </div>
         </div>
-      </div>
 
-      <template #footer>
         <div class="flex justify-end">
           <UButton
             v-if="isEdit"
+            type="button"
             size="sm"
             class="mr-2.5"
             color="red"
@@ -58,22 +62,24 @@
           </UButton>
           <UButton
             size="sm"
+            type="submit"
             :icon="isEdit ? 'i-material-symbols:save' : 'i-material-symbols:add'"
-            @click="handleClick"
           >
             {{isEdit ? 'Update': 'Create'}} card
           </UButton>
         </div>
-      </template>
+      </UForm>
     </UCard>
   </UModal>
 </template>
 
 <script setup lang="ts">
+import { object, string, type InferType } from "yup";
 import { nanoid } from "nanoid";
 import { useBoard } from "@/store/board";
 
 import type { Task } from "@/types";
+import type { FormSubmitEvent } from "#ui/types";
 
 const { createTask, updateTask, deleteTask } = useBoard();
 const modal = useModal();
@@ -96,11 +102,20 @@ const task = ref<Task>(
     } as Task
 );
 
-const handleClick = () => {
+const schema = object({
+  title: string().min(5, 'Must be at least 5 characters').required('Required'),
+  description: string().required('Required'),
+  status: string().required('Required'),
+  priority: string().required('Required'),
+});
+
+type Schema = InferType<typeof schema>
+
+function onSubmit(event: FormSubmitEvent<Schema>) {
   if (props.isEdit) {
-    updateTask(task.value);
+    updateTask(event.data as Task);
   } else {
-    createTask(task.value);
+    createTask(event.data as Task);
   }
 
   modal.close();
